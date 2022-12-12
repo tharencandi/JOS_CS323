51fa403fac91e931291552beff47a0b63e04bde8
bu-da <daniel.bucher@healthinal.com>
2022-12-12 15:57:41 +0100

    implement i/o redirection. handle keyboard/serial interrupts

--- kern/trap.c
+++ kern/trap.c
@@ -281,6 +281,13 @@ trap_dispatch(struct Trapframe *tf)
   case IRQ_OFFSET + IRQ_TIMER:
     lapic_eoi();
     sched_yield();
+    break;
+  case IRQ_OFFSET+IRQ_KBD:
+    kbd_intr();
+    break;
+  case IRQ_OFFSET+IRQ_SERIAL:
+    serial_intr();
+    break;
   break;
   default:
     // Unexpected trap: The user process or the kernel has a bug.
